85d51587d7ddb8c401f33ffdf089360020d11eb9
geekprathamesh <prathameshteli727@gmail.com>
2025-03-07 15:51:15 +0530

    updated code

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ let userscore=0;
 let compscore=0;
 const choices = document.querySelectorAll(".choice");
 
+const guesscomp = ()=>{
+    let arr = ["rock","paper","scissor"];
+    let num=Math.floor(Math.random()*3);
+    return arr[num] ;
+
+
+}
+
 choices.forEach((choice)=>{
     console.log(choice);
     choice.addEventListener("click",()=>{
